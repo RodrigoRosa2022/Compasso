d0146c20a796ff7bf45725b1fe8ba5f05538a745
Rodrigo Rosa <98928484+RodrigoRosa2022@users.noreply.github.com>
2026-08-21 12:33:55 -0300

    funcional repairs.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='compasso-static-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['index.html','style.css','app.js','manifest.json','privacy.html','icons/icon-192.png','icons/icon-512.png']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='compasso-static-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['index.html','style.css','app.js','manifest.json','privacy.html','icons/icon-192.png','icons/icon-512.png'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
