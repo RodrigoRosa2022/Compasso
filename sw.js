70686c25753ef620a9145caf247bb44c4df83133
Rodrigo Rosa <98928484+RodrigoRosa2022@users.noreply.github.com>
2026-08-21 11:51:02 -0300

    icons added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='compasso-static-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['index.html','style.css','app.js','manifest.json','privacy.html','icons/icon.svg']))));
+const CACHE='compasso-static-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['index.html','style.css','app.js','manifest.json','privacy.html','icons/icon-192.png','icons/icon-512.png']))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
